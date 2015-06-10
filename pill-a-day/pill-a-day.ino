c2bf912247220086916b5b750800f5b6183cb6d1
psytronx <roger.c.hom@gmail.com>
2015-06-10 16:41:11 -0700

    Working MVP. Detecting button press via loop. Uses delayMicroseconds for delay.

--- pill-a-day/pill-a-day.ino
+++ pill-a-day/pill-a-day.ino
@@ -1,15 +1,19 @@
 // Pill a day - reminder to take my pills every day
 
+const unsigned long DELAY_MS = 12*3600000;
 int sensorPin = A0;    // select the input pin for the potentiometer
 int ledPin = 7;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
 
 void setup() {
-  // open the serial port at 9600 bps:
   Serial.begin(9600);
   Serial.print("Start...");         
   // declare the ledPin as an OUTPUT:
   pinMode(ledPin, OUTPUT);
+  
+  // LED on by default
+  digitalWrite(ledPin, HIGH);
+  
 }
 
 void loop() {
@@ -20,9 +24,18 @@ void loop() {
   Serial.print("\n");
   
   if (sensorValue == 1){
-    digitalWrite(ledPin, HIGH);
-  } else {
     digitalWrite(ledPin, LOW);
+    waitAndTurnOnLed();
   }
   
+}
+
+void waitAndTurnOnLed() {
+  unsigned long i;
+  noInterrupts();
+  for (i = 0; i < DELAY_MS; i++){
+    delayMicroseconds(1000);
+  }
+  digitalWrite(ledPin, HIGH);
+  interrupts();
 }
